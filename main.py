6272850b0d402664cb6275d5c999c942cc86369e
Ryui-code <ulanbektalasbaev2020@gmail.com>
2026-07-17 19:46:31 +0600

    dsgsdgs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import cart, cart_item
 import uvicorn
 from admin.setup import admin_setup
@@ -8,6 +9,20 @@ app.include_router(cart.router)
 app.include_router(cart_item.router)
 
 admin_setup(app)
+
+origins = [
+    'http://localhost',
+    'http://localhost:3000',
+    'http://frontend'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
